c313072522cb7d8e94025c561c8ff4922ea77800
João Paulo de Freitas Teixeira Leite <joao.leite.740407@sga.pucminas.br>
2023-11-20 20:51:39 -0300

    Adicionar arquivos de projeto.

---
+++ ForensicBones5/Models/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForensicBones5.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Insira um e-mail válido")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        public string Cargo { get; set; }
+
+        //public ICollection<Relatorio> Relatorios { get; set; }
+
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+}
