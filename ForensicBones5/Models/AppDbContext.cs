53627d16871c67028a5c7b57dde26b5101d17535
João Paulo de Freitas Teixeira Leite <joao.leite.740407@sga.pucminas.br>
2023-11-22 23:33:16 -0300

    Descricao deletada / Controller de usuarios adicionado

--- ForensicBones5/Models/AppDbContext.cs
+++ ForensicBones5/Models/AppDbContext.cs
@@ -7,5 +7,9 @@ namespace ForensicBones5.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Relatorio> Relatorios { get; set; }
+        public DbSet<InventarioEsqueleto> InventariosEsqueleto { get; set; }
+        public DbSet<InventarioCranio> InventariosCranio { get; set; }
+        public DbSet<MarcadoresCranio> MarcadoresCranios { get; set; }
     }
 }
