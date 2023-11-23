256ab14681c8427692d7fa0603c8b25ee84d9279
Bruno Souza <bruno_ls@uol.com.br>
2023-11-22 23:49:12 -0300

    Exclusão da descrição, Adicionado DbSet, ajuste do Inventário Cranio

--- ForensicBones5/Models/AppDbContext.cs
+++ ForensicBones5/Models/AppDbContext.cs
@@ -7,5 +7,12 @@ namespace ForensicBones5.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Usuario> Relatorios { get; set; }
+
+        public DbSet<Usuario> MarcadoresCranio { get; set; }
+
+        public DbSet<Usuario> InventariosCranio { get; set; }
+
+        public DbSet<Usuario> InventarioEsqueleto { get; set; }
     }
 }
